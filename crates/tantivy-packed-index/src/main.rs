3c627fba57f8015af5c9dbc288fe4bb2009a9f6c
jcsherin <jacob@protoship.io>
2025-07-06 14:35:06 +0530

    refactor(tantivy): extract `packed_index` module for file format

--- crates/tantivy-packed-index/src/main.rs
+++ crates/tantivy-packed-index/src/main.rs
@@ -1,47 +1,68 @@
+use crate::packed_index::{FileMetadata, Header};
 use std::path::PathBuf;
 
-const MAGIC_NUMBER: u32 = u32::from_le_bytes(*b"PIX1");
+mod packed_index {
+    use std::path::PathBuf;
 
-#[allow(dead_code)]
-#[derive(Debug)]
-struct PackedIndexHeader {
-    magic_number: u32,
-    version: u16,
-    size: u64,
-    files: Vec<FileMetadata>,
-}
+    pub const MAGIC_NUMBER: u32 = u32::from_le_bytes(*b"PIX1");
 
-#[allow(dead_code)]
-#[derive(Debug)]
-struct FileMetadata {
-    path: PathBuf,
-    offset: u64,
-    length: u64,
+    #[allow(dead_code)]
+    #[derive(Debug)]
+    pub struct Header {
+        magic_number: u32,
+        version: u16,
+        size: u64,
+        files: Vec<FileMetadata>,
+    }
+
+    #[allow(dead_code)]
+    #[derive(Debug)]
+    pub struct FileMetadata {
+        path: PathBuf,
+        offset: u64,
+        length: u64,
+    }
+
+    impl Default for Header {
+        fn default() -> Self {
+            Self {
+                magic_number: MAGIC_NUMBER,
+                version: 1,
+                size: 0,
+                files: Vec::new(),
+            }
+        }
+    }
+
+    impl Header {
+        pub fn add_file_metadata(&mut self, file_metadata: FileMetadata) -> &mut Self {
+            self.files.push(file_metadata);
+            self
+        }
+    }
+
+    impl FileMetadata {
+        pub fn new(path: PathBuf, offset: u64, length: u64) -> Self {
+            Self {
+                path,
+                offset,
+                length,
+            }
+        }
+    }
 }
 
 fn main() {
-    let file1 = FileMetadata {
-        path: PathBuf::from("meta.json"),
-        offset: 0,
-        length: 256,
-    };
-    let file2 = FileMetadata {
-        path: PathBuf::from("a.idx"),
-        offset: 256,
-        length: 1024,
-    };
-    let file3 = FileMetadata {
-        path: PathBuf::from("postings.info"),
-        offset: 256 + 1024,
-        length: 4096,
-    };
+    let file1 = FileMetadata::new(PathBuf::from("meta.json"), 0, 256);
+    let file2 = FileMetadata::new(PathBuf::from("abcd.idx"), 256, 1024);
+    let file3 = FileMetadata::new(PathBuf::from("postings.info"), 256 + 1024, 4096);
 
-    let header = PackedIndexHeader {
-        magic_number: MAGIC_NUMBER, // "PKIX" as defined in the Canvas
-        version: 1,
-        size: 0,
-        files: vec![file1, file2, file3],
-    };
+    let mut header = Header::default();
+
+    header
+        .add_file_metadata(file1)
+        .add_file_metadata(file2)
+        .add_file_metadata(file3);
 
     println!("{header:#?}");
 }
