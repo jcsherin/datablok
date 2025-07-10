bda6d85b0fbb71b6c298fa6deb6dc7164cf1f3cc
jcsherin <jacob@protoship.io>
2025-07-11 01:50:46 +0530

    feat(tantivy): enrich `FileMetadata`

--- crates/tantivy-byte-array-index/src/main.rs
+++ crates/tantivy-byte-array-index/src/main.rs
@@ -12,88 +12,125 @@ use crate::index::IndexBuilder;
 use crate::query_session::QuerySession;
 use log::info;
 use query::boolean_query;
-use std::array::TryFromSliceError;
 use std::ffi::OsStr;
+use std::io::Read;
 use std::os::unix::ffi::OsStrExt;
 use std::path::PathBuf;
 use tantivy::collector::{Count, DocSetCollector};
 use tantivy::{Directory, HasLen};
 use thiserror::Error;
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Error, Debug)]
+enum FileMetadataError {
+    #[error(
+        "Malformed header detected. Header size: {bytes_len} is insufficient to hold metadata."
+    )]
+    MalformedHeader { bytes_len: usize },
+
+    #[error(
+        "Path len in header is {expected} bytes, but stored path truncated after {actual} bytes"
+    )]
+    TruncatedPath { expected: usize, actual: usize },
+
+    #[error("Parsing FileMetadata from bytes failed")]
+    ParseError(#[from] std::io::Error),
+}
+
+#[derive(Debug, Default, PartialEq, Clone)]
 struct FileMetadata {
+    /// Offset in data section from where the file contents start
+    data_offset: u64,
+    /// Length of the file contents slice stored in data section in bytes
+    data_size: u64,
+    /// Length of the file path in bytes
+    path_len: u8,
+    /// File path
     path: PathBuf,
-    file_size: u32,
 }
 
 impl FileMetadata {
-    const HEADER_SIZE_IN_BYTES: u8 = 8;
+    const DATA_OFFSET_BYTES: u64 = 8;
+    const DATA_SIZE_BYTES: u64 = 8;
+    const PATH_LEN_BYTES: u8 = 1;
 
-    pub fn new(path: PathBuf, size_in_bytes: u32) -> Self {
+    const fn header_size() -> usize {
+        Self::DATA_OFFSET_BYTES as usize
+            + Self::DATA_SIZE_BYTES as usize
+            + Self::PATH_LEN_BYTES as usize
+    }
+
+    pub fn new(path: PathBuf, data_size: u64) -> Self {
+        let path_len = path.as_os_str().as_bytes().len() as u8;
+
         Self {
             path,
-            file_size: size_in_bytes,
+            path_len,
+            data_offset: 0,
+            data_size,
         }
     }
 
-    fn to_bytes(&self, path_as_bytes: &[u8]) -> Vec<u8> {
-        let mut bytes = Vec::with_capacity(4 + 4 + path_as_bytes.len());
+    fn to_bytes(&self, path: &[u8]) -> Vec<u8> {
+        let mut bytes = Vec::with_capacity(Self::header_size() + path.len());
 
-        let path_len = path_as_bytes.len() as u32;
-
-        bytes.extend(path_len.to_le_bytes());
-        bytes.extend(self.file_size.to_le_bytes());
-        bytes.extend(path_as_bytes);
+        bytes.extend(self.data_offset.to_le_bytes());
+        bytes.extend(self.data_size.to_le_bytes());
+        bytes.extend(self.path_len.to_le_bytes());
+        bytes.extend(path);
 
         bytes
     }
 }
 
+const FILE_METADATA_HEADER_SIZE: usize = FileMetadata::header_size();
+
 impl From<FileMetadata> for Vec<u8> {
     fn from(value: FileMetadata) -> Self {
-        let path_as_bytes = value.path.as_os_str().as_bytes();
-
-        value.to_bytes(path_as_bytes)
+        value.to_bytes(value.path.as_os_str().as_bytes())
     }
 }
 
-#[derive(Error, Debug)]
-enum FileMetadataError {
-    #[error("Header does not contain path length and data size.")]
-    IncompleteHeader,
-
-    #[error("Path name got truncated.")]
-    PathNameTruncated,
-
-    #[error("Failed to convert slice to array")]
-    ConversionError(#[from] TryFromSliceError),
-}
-
 impl TryFrom<Vec<u8>> for FileMetadata {
-    type Error = Box<dyn std::error::Error>;
+    type Error = FileMetadataError;
 
     fn try_from(bytes: Vec<u8>) -> std::result::Result<Self, Self::Error> {
-        let input_size = bytes.len();
-
-        if input_size < FileMetadata::HEADER_SIZE_IN_BYTES as usize {
-            return Err(Box::new(FileMetadataError::IncompleteHeader));
+        if bytes.len() < FILE_METADATA_HEADER_SIZE {
+            return Err(FileMetadataError::MalformedHeader {
+                bytes_len: bytes.len(),
+            });
         }
 
-        let path_len_bytes: [u8; 4] = bytes[0..4].try_into()?;
-        let path_len = u32::from_le_bytes(path_len_bytes) as usize;
+        let mut cursor = std::io::Cursor::new(bytes);
 
-        let file_size_bytes: [u8; 4] = bytes[4..8].try_into()?;
-        let file_size = u32::from_le_bytes(file_size_bytes);
+        let mut u64_buffer = [0u8; 8];
+        cursor.read_exact(&mut u64_buffer)?;
+        let data_offset = u64::from_le_bytes(u64_buffer);
 
-        if input_size < (FileMetadata::HEADER_SIZE_IN_BYTES as usize + path_len) {
-            return Err(Box::new(FileMetadataError::PathNameTruncated));
+        cursor.read_exact(&mut u64_buffer)?;
+        let data_size = u64::from_le_bytes(u64_buffer);
+
+        let mut path_len_buffer = [0u8; 1];
+        cursor.read_exact(&mut path_len_buffer)?;
+        let path_len = u8::from_le_bytes(path_len_buffer);
+
+        let remaining_len = cursor.get_ref().len() - (cursor.position() as usize);
+        if remaining_len < path_len.into() {
+            return Err(FileMetadataError::TruncatedPath {
+                expected: path_len.into(),
+                actual: remaining_len,
+            });
         }
 
-        let path_bytes = &bytes[8..(8 + path_len)];
-        let os_str = OsStr::from_bytes(path_bytes);
-        let path = PathBuf::from(os_str);
+        let mut path_buffer = vec![0u8; path_len as usize];
+        cursor.read_exact(&mut path_buffer)?;
+        let path = PathBuf::from(OsStr::from_bytes(&path_buffer));
 
-        Ok(Self { path, file_size })
+        Ok(Self {
+            data_offset,
+            data_size,
+            path_len,
+            path,
+        })
     }
 }
 
@@ -118,7 +155,7 @@ fn main() -> Result<()> {
     let dir = index.directory();
 
     for path in dir.list_managed_files() {
-        let size_in_bytes = if path.eq(&metadata_file) {
+        let data_size = if path.eq(&metadata_file) {
             let contents = dir
                 .atomic_read(&path)
                 .unwrap_or_else(|e| panic!("Error: {e} while reading metadata file: {path:?}"));
@@ -132,10 +169,10 @@ fn main() -> Result<()> {
             file_slice.len()
         };
 
-        let file_metadata = FileMetadata::new(path.clone(), size_in_bytes as u32);
+        let file_metadata = FileMetadata::new(path.clone(), data_size as u64);
 
         info!(
-            "Path size={0}, size in bytes={size_in_bytes}, path={path:?}",
+            "Path size={0}, size in bytes={data_size}, path={path:?}",
             path.as_os_str().len()
         );
 
@@ -177,6 +214,7 @@ fn setup_logging() {
 #[cfg(test)]
 mod tests {
     use crate::FileMetadata;
+    use std::os::unix::ffi::OsStrExt;
     use std::path::PathBuf;
 
     #[test]
@@ -193,6 +231,7 @@ mod tests {
     #[test]
     fn file_metadata_incomplete_header_error() {
         let bytes: Vec<u8> = vec![1, 2, 3, 4, 5];
+        let header_size = bytes.len();
 
         let result: Result<FileMetadata, _> = bytes.try_into();
 
@@ -201,24 +240,42 @@ mod tests {
         let err = result.unwrap_err();
         assert_eq!(
             err.to_string(),
-            "Header does not contain path length and data size."
+            format!(
+                "Malformed header detected. Header size: {header_size} is insufficient to hold metadata.",
+            )
         );
     }
 
     #[test]
     fn file_metadata_truncated_data_error() {
-        // Path length is 50 bytes but the data is only 5 bytes
-        let bytes: Vec<u8> = vec![
-            50, 0, 0, 0, // Path length = 50
-            10, 0, 0, 0, // File size = 10
-            1, 2, 3, 4, 5, // Only 5 bytes of path data
-        ];
+        // Path length is 42 bytes but there is only 8 bytes of path data
+        let data_offset_bytes = u64::to_le_bytes(200);
+        let data_size_bytes = u64::to_le_bytes(1024);
+
+        let path_length = 42;
+        let path_length_bytes = u8::to_le_bytes(path_length);
+
+        let path = PathBuf::from("meta.json");
+        let path_bytes = path.as_os_str().as_bytes();
+
+        let mut bytes: Vec<u8> = Vec::new();
+
+        bytes.extend(data_offset_bytes);
+        bytes.extend(data_size_bytes);
+        bytes.extend(path_length_bytes);
+        bytes.extend(path_bytes);
 
         let result: Result<FileMetadata, _> = bytes.try_into();
 
         assert!(result.is_err());
 
         let err = result.unwrap_err();
-        assert_eq!(err.to_string(), "Path name got truncated.");
+        assert_eq!(
+            err.to_string(),
+            format!(
+                "Path len in header is {path_length} bytes, but stored path truncated after {} bytes",
+                path_bytes.len()
+            )
+        );
     }
 }
