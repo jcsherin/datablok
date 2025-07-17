570ddfd1d4d4221f4c701d3878a36bc59b67a39a
jcsherin <jacob@protoship.io>
2025-07-17 14:58:08 +0530

    chore(log): make log level configurable via environment

--- crates/tantivy-byte-array-index/src/main.rs
+++ crates/tantivy-byte-array-index/src/main.rs
@@ -766,7 +766,5 @@ fn main() -> Result<()> {
 
 /// Initializes the logger.
 fn setup_logging() {
-    env_logger::builder()
-        .filter_level(log::LevelFilter::Info)
-        .init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 }
