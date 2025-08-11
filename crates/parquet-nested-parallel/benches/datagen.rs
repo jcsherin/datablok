a9dc1e47b7a42a33f44e0789c6b49302a7879274
Sherin Jacob <jacob@protoship.io>
2025-08-11 14:45:15 +0530

    feat(parquet-nested-parallel): Add benchmark for RecordBatch generation

--- crates/parquet-nested-parallel/benches/datagen.rs
+++ crates/parquet-nested-parallel/benches/datagen.rs
@@ -1,16 +1,20 @@
 use criterion::{Criterion, criterion_group, criterion_main};
-use parquet_parallel_nested::skew::generate_name;
-use parquet_parallel_nested::skew::generate_phone_template;
+use parquet_nested_common::prelude::get_contact_schema;
+use parquet_nested_parallel::datagen::ContactRecordBatchGenerator;
+use parquet_nested_parallel::skew::{generate_name, generate_phone_template};
 use rand::SeedableRng;
 use rand::prelude::StdRng;
 use std::hint::black_box;
+use std::sync::Arc;
+use std::sync::atomic::AtomicUsize;
 use std::time::Duration;
 
-fn criterion_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("nested data generator");
+/// Benchmarks for primitive data generation functions in `skew.rs`.
+fn skew_primitives_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("skew primitives generator");
     group.measurement_time(Duration::from_secs(20));
 
-    const SEED: u64 = 123456;
+    const SEED: u64 = 0;
     const NAME_SIZE: usize = 32;
     let rng = &mut StdRng::seed_from_u64(SEED);
     let mut name_buf = String::with_capacity(NAME_SIZE);
@@ -24,5 +28,27 @@ fn criterion_benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+/// Benchmarks for generating full `RecordBatch` of contacts.
+fn contact_record_batch_generator_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("ContactRecordBatchGenerator");
+    group.measurement_time(Duration::from_secs(20));
+
+    let schema = get_contact_schema();
+    const SEED: u64 = 0;
+    const RECORD_BATCH_SIZES: &[usize] = &[1024, 2048, 4096, 8192];
+
+    for &size in RECORD_BATCH_SIZES {
+        group.bench_function(format!("generate {} records", size), |b| {
+            let counter = Arc::new(AtomicUsize::new(0));
+            let mut generator = ContactRecordBatchGenerator::new(schema.clone(), counter);
+            b.iter(|| black_box(generator.generate(SEED, size).unwrap()))
+        });
+    }
+}
+
+criterion_group!(
+    benches,
+    skew_primitives_benchmark,
+    contact_record_batch_generator_benchmark
+);
 criterion_main!(benches);
